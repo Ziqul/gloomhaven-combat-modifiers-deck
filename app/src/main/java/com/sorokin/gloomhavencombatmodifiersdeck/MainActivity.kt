168eccb34ec82b556b0f4ea4ade6117c89f4c940
Ziqul <m.s.v.00a@gmail.com>
2018-12-26 13:56:32 +0200

    New icon. Add cards images. Delete junk
    Changed icon to swords from card background.
    Added to drawable scans of all cards.
    Deleted all empty folders and unnecessary resources.

--- app/src/main/java/com/sorokin/gloomhavencombatmodifiersdeck/MainActivity.kt
+++ app/src/main/java/com/sorokin/gloomhavencombatmodifiersdeck/MainActivity.kt
@@ -2,11 +2,20 @@ package com.sorokin.gloomhavencombatmodifiersdeck
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.setContentView(R.layout.activity_main)
+
+        next_card_button.setOnClickListener {
+            shown_card.setImageResource(R.drawable.card_back)
+        }
+
+        shuffle_button.setOnClickListener {
+            shown_card.setImageResource(0)
+        }
     }
 }
