ea86b56caca510e9bd3656dfdf26e2790ba5cedc
Ziqul <m.s.v.00a@gmail.com>
2018-12-26 17:35:53 +0200

    Added default deck definition. Implemented random cards display

--- app/src/main/java/com/sorokin/gloomhavencombatmodifiersdeck/MainActivity.kt
+++ app/src/main/java/com/sorokin/gloomhavencombatmodifiersdeck/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sorokin.gloomhavencombatmodifiersdeck
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +11,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_main)
 
+        val defaultDeckData: Array<String> =
+                resources.getStringArray(R.array.default_deck)
+
+        val defaultDeck: ArrayList<String> = arrayListOf()
+        defaultDeckData.forEach {
+            val amount: Int = it.split(Regex("\\|")).get(1).toInt()
+            for (i in 1..amount) {
+                defaultDeck.add(it.split(Regex("\\|")).get(0))
+            }
+        }
+
+        cards_left_value.text = "" + defaultDeck.size
+
         next_card_button.setOnClickListener {
-            shown_card.setImageResource(R.drawable.card_back)
+            if (defaultDeck.size > 0) {
+                shown_card.setImageResource(
+                    resources.getIdentifier(
+                        defaultDeck.removeAt(Random().nextInt(defaultDeck.size)),
+                        "drawable",
+                        "com.sorokin.gloomhavencombatmodifiersdeck"))
+            }
+
+            if (defaultDeck.size < 1) {
+                hidden_card.setImageResource(R.drawable.card_transparent)
+            }
+
+            cards_left_value.text = "" + defaultDeck.size
         }
 
         shuffle_button.setOnClickListener {
-            shown_card.setImageResource(0)
+            hidden_card.setImageResource(R.drawable.card_back)
+            shown_card.setImageResource(R.drawable.card_transparent)
+            defaultDeck.clear()
+            defaultDeckData.forEach {
+                val amount: Int = it.split(Regex("\\|")).get(1).toInt()
+                for (i in 1..amount) {
+                    defaultDeck.add(it.split(Regex("\\|")).get(0))
+                }
+            }
+
+            cards_left_value.text = "" + defaultDeck.size
         }
     }
 }
